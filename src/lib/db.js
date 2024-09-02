4450dad22f43ef27952dab336ce4b229fbacbd83
ShivamGuj <89210077+ShivamGuj@users.noreply.github.com>
2024-09-02 13:31:47 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,21 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB Connected");
   } catch (err) {
-    console.error(err.message);
+    console.error("MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
 
 export default connectDB;
+
